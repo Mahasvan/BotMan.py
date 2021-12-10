17705c96b333967ba3014f41c93c37bf95a76db9
Mahas1 <mahasvan@gmail.com>
2021-12-10 15:22:27 +0530

    add error logging to db_manager.py

--- assets/db_manager.py
+++ assets/db_manager.py
@@ -19,14 +19,23 @@ class DbManager:
         self.setup_table()
 
     def setup_table(self):
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS prefixes (id INTEGER PRIMARY KEY, prefix VARCHAR(10))""")
+        try:
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS prefixes (id INTEGER PRIMARY KEY, prefix VARCHAR(10))""")
+        except Exception as e:
+            self.bot.logger.log_error(e, "setup_table")
 
     def remove_guild_prefix(self, guild_id: int):
-        self.cursor.execute(f"""DELETE FROM prefixes WHERE id = {guild_id}""")
+        try:
+            self.cursor.execute(f"""DELETE FROM prefixes WHERE id = {guild_id}""")
+        except Exception as e:
+            self.bot.logger.log_error(e, "remove_guild_prefix")
 
     def add_guild_prefix(self, guild_id: int, prefix: str):
         self.remove_guild_prefix(guild_id)
-        self.cursor.execute(f"""INSERT INTO prefixes VALUES({guild_id}, \"{prefix}\")""")
+        try:
+            self.cursor.execute(f"""INSERT INTO prefixes VALUES({guild_id}, \"{prefix}\")""")
+        except Exception as e:
+            self.bot.logger.log_error(e, "add_guild_prefix")
 
     def get_guild_prefix(self, guild_id: int):
         self.cursor.execute(f"""SELECT prefix FROM prefixes WHERE id = {guild_id}""")
@@ -37,6 +46,6 @@ class DbManager:
             if isinstance(e, TypeError):
                 return self.bot.default_prefix
             else:
-                # TODO: Log error to file
+                self.bot.logger.log_error(e, "get_guild_prefix")
                 print(type(e).__name__, e)
                 return self.bot.default_prefix
