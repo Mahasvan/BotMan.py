5ff3c95f695125a71d8be804b803536d603443be
Mahas1 <mahasvan@gmail.com>
2021-12-11 15:40:13 +0530

    fix logger not logging error events

--- assets/logger.py
+++ assets/logger.py
@@ -4,16 +4,18 @@ from datetime import datetime
 class Logger:
 
     def __init__(self, logfile):
-        self.logfile = logfile
-        self.log = open(logfile, 'a')
+        self.logfile_path = logfile
+        self.logfile = open(logfile, 'a')
         self.log_info("Logger initialized")
 
     def log_error(self, error: Exception, file_or_command: str = "N/A"):
         dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         error_message = f"{dt_string} | {file_or_command} | ERROR | {type(error).__name__} | {error}"
-        self.log.write(error_message + "\n")
+        self.logfile.write(error_message + "\n")
+        self.logfile.flush()
 
     def log_info(self, info: str, file_or_command="N/A"):
         dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         info_message = f"{dt_string} | {file_or_command} | INFO | {info}"
-        self.log.write(info_message + "\n")
+        self.logfile.write(info_message + "\n")
+        self.logfile.flush()
