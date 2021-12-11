5f17cb515941b556f04ea27d1fa09f08496b310b
Mahas1 <mahasvan@gmail.com>
2021-12-11 15:22:26 +0530

    add useful checks to discord_funcs.py

--- assets/discord_funcs.py
+++ assets/discord_funcs.py
@@ -1,4 +1,5 @@
 import discord
+import json
 
 
 def get_color(user: discord.Member):
@@ -6,3 +7,33 @@ def get_color(user: discord.Member):
     if str(color) == "#000000":
         color = discord.Color.random()
     return color
+
+
+def check_muted_role(ctx):
+    with open(f'./configs/guild{ctx.guild.id}.json', 'r') as jsonFile:
+        data = json.load(jsonFile)
+    muted_role = data.get('mute_role')
+    if muted_role is None:
+        muted_role = 'muted'  # check for muted role in the guild's config, failsafe: "muted"
+
+    x = ctx.guild.roles
+    is_present = False
+    for y in x:  # iterate over roles and find muted role
+        if y.name == muted_role:
+            is_present = True
+            break
+    return is_present
+
+
+def is_author(ctx, user):
+    if ctx.author.id == user.id:
+        return True
+    else:
+        return False
+
+
+def is_client(client, user):
+    if client.user.id == user.id:
+        return True
+    else:
+        return False
