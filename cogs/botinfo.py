06913e643592a3672955e3bdd61e0021136ea7d5
Mahas1 <mahasvan@gmail.com>
2022-01-08 10:07:14 +0530

    remove cpu/ram usage thereby eliminating need for psutil

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -4,7 +4,6 @@ import random
 import time
 
 import discord
-import psutil
 import speedtest
 from discord.ext import commands
 
@@ -145,22 +144,13 @@ class BotInfo(commands.Cog, description="Information on various aspects of the b
     @commands.command(name="hostinfo", description="Returns information about my host.")
     async def hostinfo(self, ctx):
         system = platform.uname()
-        cpu_usage = psutil.cpu_percent()
-        mem_stats = psutil.virtual_memory()
-        memUsedGB = "{0:.1f}".format(((mem_stats.used / 1024) / 1024) / 1024)  # Credit to @CorpNewt for this section
-        memTotalGB = "{0:.1f}".format(((mem_stats.total / 1024) / 1024) / 1024)
         processor = str(system.processor) if str(system.processor) != "" else "N/A"
         embed = discord.Embed(title=f"Host Name: {system.node}",
-                              description=f"Platform: {system.system} | Version: {system.version}",
+                              description=f"Platform: {system.system} {system.release}",
                               color=get_color(ctx.guild.me))
-        embed.add_field(name="Release", value=system.release, inline=True)
-        embed.add_field(name="Machine Type", value=system.machine, inline=True)
-        embed.add_field(name="Threads", value=str(os.cpu_count()), inline=True)
+        embed.add_field(name="Machine Type", value=system.machine, inline=False)
         embed.add_field(name="CPU", value=processor, inline=False)
-        embed.add_field(name="CPU Frequency", value=f"{int(list(psutil.cpu_freq())[0])} MHz", inline=True)
-        embed.add_field(name="CPU Usage", value=f"{cpu_usage}%", inline=True)
-        embed.add_field(name="RAM Usage", value=f"{memUsedGB} GB of {memTotalGB} GB ({mem_stats.percent}%)", inline=True)
-
+        embed.add_field(name="CPU Threads", value=str(os.cpu_count()), inline=False)
         await ctx.send(embed=embed)
 
 
