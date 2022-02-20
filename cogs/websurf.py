d375d39fdf15662eee5441164dfd27a451fa95d1
Mahas1 <mahasvan@gmail.com>
2022-02-20 13:58:51 +0530

    add new commands based on reddit

--- cogs/websurf.py
+++ cogs/websurf.py
@@ -175,52 +175,46 @@ class WebSurf(commands.Cog, description='Fun commands using the Reddit API, and 
         embed.set_footer(text=f"Author - {define1.get('author')} | Powered by Urban Dictionary")
         await ctx.send(embed=embed)
 
+    @commands.command(name="google", aliases=["search", "lmgtfy"])
+    async def lmgtfy(self, ctx, *, search_term):
+        """Feeling too lazy to open a browser? Search here!"""
+        encoded_term = urllib.parse.quote(search_term)
+        lmgtfy_link = f"https://lmgtfy.app/?q={encoded_term}"
+        tinyurl_link = await internet_funcs.get_response(f"https://tinyurl.com/api-create.php?url={lmgtfy_link}")
+        await ctx.send(f"_{ctx.author.display_name}_, here is the google search you asked for.\n"
+                       f"<{tinyurl_link}>")
 
-#
-#     @commands.command(name='convert', description='Converts an integer value from one currency to another.\n'
-#                                                   f'Usage example: `bm-convert 100 USD EUR`\n'
-#                                                   f'For currency codes, check '
-#                                                   f'__[here](https://www.iban.com/currency-codes)__')
-#     async def get_currency(self, ctx, value: float, from_currency: str.upper, to_currency: str.upper):
-#         async with ctx.typing():
-#             final_string = await money_convert.get_converted_currency(value, from_currency, to_currency)
-#             await ctx.send(final_string)
-#
-#     @commands.command(name="google", aliases=["search"], description="Feeling too lazy to open a browser? Search here!")
-#     async def lmgtfy(self, ctx, *, search_term):
-#         encoded_term = urllib.parse.quote(search_term)
-#         lmgtfy_link = f"https://lmgtfy.app/?q={encoded_term}"
-#         tinyurl_link = await tinyurl.get_tinyurl(lmgtfy_link)
-#         await ctx.send(f"_{ctx.author.display_name}_, here is the google search you asked for.\n"
-#                        f"<{tinyurl_link}>")
-#
-#     @commands.command(name="tinyurl", aliases=["tiny"], description="URL shortening command.")
-#     async def tinyurl_command(self, ctx, *, url):
-#         if not url.startswith("http"):
-#             url = f"https://{url}"
-#         message = await ctx.send(f"Shortening **{url}**...\n")
-#         try:
-#             tinyurl_link = await tinyurl.get_tinyurl(url)
-#         except Exception as e:
-#             return await message.edit(content=f"Could not shorten URL.\n{e if len(str(e)) < 1024 else str(e)[:1024]}")
-#         await message.edit(content=f"Here is the shortened URL for **{url}**.\n{tinyurl_link}")
-#
-#     @commands.command(name="wallpaper", aliases=["wall"], description="Fetches wallpaper from r/wallpaper")
-#     async def wallpaper_command(self, ctx):
-#         await ctx.invoke(self.get_reddit_post, "wallpaper")
-#
-#     @commands.command(name="showerthought", aliases=["shower"], description="Fetches showerthought from r/showerthoughts")
-#     async def showerthought_command(self, ctx):
-#         subreddit = await reddit.subreddit("showerthoughts")  # get the subreddit
-#         x = subreddit.hot(limit=10)  # get the top 10 hot posts
-#         title_list = []
-#         async for y in x:
-#             title_list.append(str(y.title))
-#         choice = random.choice(title_list)
-#         embed = discord.Embed(title=choice, color=discord.Colour.random())
-#         embed.set_footer(text="Powered by r/showerthoughts")
-#         await ctx.send(embed=embed)
-#
-#
+    @commands.command(name="tinyurl", aliases=["tiny"])
+    async def tinyurl_command(self, ctx, *, url):
+        """Shortens a URL with TinyURL"""
+        if not url.startswith("http"):
+            url = f"https://{url}"
+        message = await ctx.send(f"Shortening **{url}**...\n")
+        try:
+            tinyurl_link = await internet_funcs.get_response(f"https://tinyurl.com/api-create.php?url={url}")
+        except:
+            return await message.edit(content=f"Something went wrong! I couldn't shorten that URL. :(")
+        await message.edit(content=f"Here is the shortened URL for **{url}**.\n{tinyurl_link}")
+
+    @commands.command(name="wallpaper", aliases=["wall"])
+    async def wallpaper_command(self, ctx):
+        """Fetches wallpaper from r/wallpaper"""
+        await ctx.trigger_typing()
+        await ctx.invoke(self.get_reddit_post, "wallpaper")
+
+    @commands.command(name="showerthought", aliases=["shower"])
+    async def showerthought_command(self, ctx):
+        """Shower thought, fresh from r/showerthoughts"""
+        await ctx.trigger_typing()
+        subreddit = await self.bot.reddit.subreddit("showerthoughts")  # get the subreddit
+        x = subreddit.hot(limit=10)  # get the top 10 hot posts
+        title_list = []
+        async for y in x:
+            title_list.append(str(y.title))
+        choice = random.choice(title_list)
+        embed = discord.Embed(title=choice, color=discord.Colour.random())
+        await ctx.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(WebSurf(bot))
