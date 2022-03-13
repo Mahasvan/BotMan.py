0d0e9a29479dd0993e0cec0add380ebc7da6ca3b
Mahas1 <mahasvan@gmail.com>
2022-03-13 19:50:22 +0530

    overhaul the logging module

--- assets/logger.py
+++ assets/logger.py
@@ -1,17 +1,26 @@
 import asyncio
 from datetime import datetime
+import json
 
 import discord
 
 
 class Logger:
 
-    def __init__(self, bot, logfile_path):
+    def __init__(self, bot, logfile_path="botman.log", logfile_json_path="botman.json"):
         self.bot = bot
         self.logfile_path = logfile_path
+        self.logfile_json_path = logfile_json_path
         self.logfile = open(self.logfile_path, 'a')
+        self.log_to_channel = True
+
+        try:
+            self.json_log_object = json.load(open(logfile_json_path, 'r'))
+        except:
+            self.json_log_object = {}
+        self.logfile_json = open(logfile_json_path, 'w')
+
         self.log_info("Logger initialized")
-        self.logfile.write("\n")
 
     def send_message(self, message=None, embed=None):
         if not self.bot.log_channel:
@@ -26,10 +35,22 @@ class Logger:
     def log_error(self, error: Exception, file_or_command: str = "N/A", send_message=True):
         dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         error_message = f"{dt_string} | {file_or_command} | ERROR | {type(error).__name__} | {error}"
+        error_dict = {
+            "type": "error",
+            "timestamp": dt_string,
+            "file_or_command": file_or_command,
+            "error_type": type(error).__name__,
+            "message": str(error)
+        }
         self.logfile.write(error_message + "\n")
         self.logfile.flush()
-        if self.bot.log_channel and send_message:
-            embed = discord.Embed(title="Error", description=error_message, color=0xFF0000)
+        self.json_log_object[dt_string] = error_dict
+        self.clear_logfile_json(send_message=False)  # clear out the file because it doesn't overwrite the previous data
+        json.dump(self.json_log_object, self.logfile_json, indent=4)
+        self.logfile_json.flush()
+        if self.bot.log_channel and send_message and self.log_to_channel:
+            embed = discord.Embed(title=f"Error: {file_or_command}", description=error, color=0xFF0000)
+            embed.set_footer(text=dt_string)
             self.send_message(embed=embed)
 
     def log_info(self, info: str, file_or_command="N/A", send_message=True):
@@ -37,8 +58,19 @@ class Logger:
         info_message = f"{dt_string} | {file_or_command} | INFO | {info}"
         self.logfile.write(info_message + "\n")
         self.logfile.flush()
-        if self.bot.log_channel and send_message:
-            embed = discord.Embed(title="Info", description=info_message, color=0xFFFF00)
+        info_dict = {
+            "type": "info",
+            "timestamp": dt_string,
+            "file_or_command": file_or_command,
+            "message": str(info)
+        }
+        self.json_log_object[dt_string] = info_dict
+        self.clear_logfile_json(send_message=False)  # clear out the file because it doesn't overwrite the previous data
+        json.dump(self.json_log_object, self.logfile_json, indent=4)
+        self.logfile_json.flush()
+        if self.bot.log_channel and send_message and self.log_to_channel:
+            embed = discord.Embed(title=f"Info: {file_or_command}", description=info, color=0xFFFF00)
+            embed.set_footer(text=dt_string)
             self.send_message(embed=embed)
 
     def clear_logfile(self, send_message=True):
@@ -46,27 +78,29 @@ class Logger:
         self.logfile = open(self.logfile_path, 'w')
         self.logfile.close()
         self.logfile = open(self.logfile_path, 'a')
-        if self.bot.log_channel and send_message:
+        if self.bot.log_channel and send_message and self.log_to_channel:
             embed = discord.Embed(title="Cleared logfile!", color=0x000000)
             self.send_message(embed=embed)
 
-    def retrieve_log(self, message_count: int = 5):
-        with open(self.logfile_path, 'r') as logfile:
-            log_lines = logfile.readlines()
-            log_lines.reverse()
-        checking = True
-        while checking:
-            for line in range(len(log_lines)):
-                if "|" not in str(log_lines[line]):
-                    log_lines.pop(line)
-                    break
-                if log_lines[line].strip() == "\n":
-                    log_lines.pop(line)
-                    break
-                if log_lines[line].strip() == "":
-                    log_lines.pop(line)
-                    break
-            else:
-                checking = False
-        to_return = [line.strip("\n") for line in log_lines[:message_count][::-1]]
+    def clear_logfile_json(self, send_message=True):
+        self.logfile_json.close()
+        self.logfile_json = open(self.logfile_json_path, 'w')
+        self.logfile_json.close()
+        self.logfile_json = open(self.logfile_json_path, 'w')
+        if self.bot.log_channel and send_message and self.log_to_channel:
+            embed = discord.Embed(title="Cleared JSON logfile!", color=0x000000)
+            self.send_message(embed=embed)
+
+    def retrieve_log_json(self, message_count: int = 5, log_type="all"):
+        """Accepted values of log_type: all, error, info"""
+        to_return = []
+        time_list = sorted(list(self.json_log_object.keys()))
+        time_list.reverse()
+        try:
+            for time in time_list[:message_count]:
+                details = self.json_log_object[time]
+                if details["type"] == log_type or log_type == "all":
+                    to_return.append(details)
+        except TypeError:  # no logs found
+            return to_return
         return to_return
